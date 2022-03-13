9c57130e274768fc5617a86d83c65692230f162f
tkmszk <takumisuzuki01@gmail.com>
2022-03-13 12:37:23 +0000

    test: 統合テストにSing upページを追加

--- src/test/integration/site_layout_test.rb
+++ src/test/integration/site_layout_test.rb
@@ -8,7 +8,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', signup_path
+
+    # Contact
     get contact_path
     assert_select 'title', full_title('Contact')
+
+    # Sign up
+    get signup_path
+    assert_select 'title', full_title('Sign up')
   end
 end
