2b6acc133352220049f243ada4f8e2b3ed53a382
tkmszk <takumisuzuki01@gmail.com>
2022-03-13 12:31:34 +0000

    update: routing変更, ログイン関係

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'users#loging'
 end
