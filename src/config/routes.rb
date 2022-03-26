d7e27cc6742834d262d3981d231ee1d87df5ce39
tkmszk <takumisuzuki01@gmail.com>
2022-03-26 12:15:04 +0000

    update: signupのフォームを/signupに送信するよう変更

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#new'
   get '/login', to: 'users#loging'
   resources :users
 end
