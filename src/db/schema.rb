b2316d46120c8db77cc293925150882feb4ea5a7
tkmszk <takumisuzuki01@gmail.com>
2022-03-26 02:54:53 +0000

    update: validation追加

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_001629) do
+ActiveRecord::Schema.define(version: 2022_03_26_025044) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
