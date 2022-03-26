3a96c2be6e7ba434fd5fb2e600b393142eaa2e44
tkmszk <takumisuzuki01@gmail.com>
2022-03-26 03:05:12 +0000

    refactor: autoformat

--- src/test/models/user_test.rb
+++ src/test/models/user_test.rb
@@ -57,7 +57,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email addresses should be unique" do
+  test 'email addresses should be unique' do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
