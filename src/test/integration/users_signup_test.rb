3269c003c960ce5f290eddc0fe5647a5c72b55db
tkmszk <takumisuzuki01@gmail.com>
2022-03-26 12:13:12 +0000

    test: エラーメッセージのテスト追加

--- src/test/integration/users_signup_test.rb
+++ src/test/integration/users_signup_test.rb
@@ -14,5 +14,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 end
