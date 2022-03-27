1d805825cf780430bd174f7ac65fab4aa9498a47
tkmszk <takumisuzuki01@gmail.com>
2022-03-27 12:00:24 +0000

    update: PW再設定フォームの画面を追加

--- src/app/controllers/password_resets_controller.rb
+++ src/app/controllers/password_resets_controller.rb
@@ -1,20 +1,33 @@
 class PasswordResetsController < ApplicationController
-  def new
-  end
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+
+  def new; end
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = 'Email sent with password reset instructions'
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = 'Email address not found'
       render 'new'
     end
   end
 
-  def edit
+  def edit; end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
   end
 end
