173faf65c5d29ec5228a4c806b51a49542a1c901
tkmszk <takumisuzuki01@gmail.com>
2022-03-27 15:10:02 +0000

    update: homeにフィード追加

--- src/app/controllers/static_pages_controller.rb
+++ src/app/controllers/static_pages_controller.rb
@@ -1,14 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
-  def help
-  end
+  def help; end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
 end
