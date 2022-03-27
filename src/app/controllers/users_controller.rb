ed679329b19aa9286145bc9e22f69cbc35516fca
tkmszk <takumisuzuki01@gmail.com>
2022-03-27 14:46:42 +0000

    update: Micropostsのcreateとdestroy追加

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -57,14 +57,6 @@ class UsersController < ApplicationController
     )
   end
 
-  private def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
-  end
-
   private def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
