3f7fe65b07077faf91c6c0409d06d13b846a3bbd
tkmszk <takumisuzuki01@gmail.com>
2022-03-28 14:56:30 +0000

    add test: follow/unfollowに関する統合テスト追加

--- src/test/models/user_test.rb
+++ src/test/models/user_test.rb
@@ -108,4 +108,25 @@ class UserTest < ActiveSupport::TestCase
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
+
+  test 'feed should have the right posts' do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+
+    # フォローしているユーザーの投稿を含むこと
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+
+    # 自分自身の投稿を含むこと
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+
+    # フォローしていないユーザーの投稿を含まないこと
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
